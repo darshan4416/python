99443bfe9ed6138a59272b0834e83f7129f1d1fe
darshan4416 <darshanhirekurubar4416@gmail.com>
2022-01-31 12:45:33 +0530

    drf home and post methods added

--- DRF/app/views.py
+++ DRF/app/views.py
@@ -1,3 +1,27 @@
+import imp
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+@api_view(['GET'])
+def home(request):
+    students_objs = student.objects.all()
+
+    serializer = studentSerializer(students_objs, many=True)
+    return Response({'status':200,'payload':serializer.data})
+
+@api_view(['POST'])
+def post(request):
+    data = request.data
+    serializer = studentSerializer(data = request.data)
+
+    if not serializer.is_valid():
+        return Response({'status':403,'message':'something went wrong'})
+
+    serializer.save()
+    
+    return Response({'status':200,'payload':serializer.data,'message':'data saved successfully'})
+
